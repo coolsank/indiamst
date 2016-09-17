de4ff44b43c616932954c3c51eaa598e7fbe214c
Sudharsan Vaidhun <vbsudharsan@gmail.com>
2016-09-17 12:49:07 -0500

    Add Freshers Photo

--- js/freshers_2016_data.js
+++ js/freshers_2016_data.js
@@ -2,9 +2,25 @@ var freshers_data = {
 	album_name : "Freshers' Bash 2016",
 	photo : [
 		{
-			image_src : "img/freshers/sample.jpg",
+			image_src : "img/freshers/image01.jpg",
+			name : "..."
+		},
+		{
+			image_src : "img/freshers/image02.jpg",
+			name : "..."
+		},
+		{
+			image_src : "img/freshers/image03.jpg",
+			name : "..."
+		},
+		{
+			image_src : "img/freshers/image04.jpg",
+			name : "..."
+		},
+		{
+			image_src : "img/freshers/image05.jpg",
 			name : "..."
 		}
 	],
-	image_credits : "Anon"
+	image_credits : "Swathi, Pavithra"
 }
