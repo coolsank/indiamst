b0c68be6be4d4351ce5de0e27bb7e8e21fcbe4c0
Sudharsan Vaidhun <svqm5@mst.edu>
2016-10-03 14:09:17 -0500

    Add Independance day Pics and Data

---
+++ js/independance_2016_data.js
@@ -0,0 +1,42 @@
+var freshers_data = {
+	album_name : "Independence Day 2016",
+	photo : [
+		{
+			image_src : "img/independance/image01.jpg",
+			name : "..."
+		},
+		{
+			image_src : "img/independance/image02.jpg",
+			name : "..."
+		},
+		{
+			image_src : "img/independance/image03.jpg",
+			name : "..."
+		},
+		{
+			image_src : "img/independance/image04.jpg",
+			name : "..."
+		},
+		{
+			image_src : "img/independance/image05.jpg",
+			name : "..."
+		},
+		{
+			image_src : "img/independance/image06.jpg",
+			name : "..."
+		},
+		{
+			image_src : "img/independance/image07.jpg",
+			name : "..."
+		},
+		{
+			image_src : "img/independance/image08.jpg",
+			name : "..."
+		},
+		{
+			image_src : "img/independance/image09.jpg",
+			name : "..."
+		}
+	],
+	image_credits : "Swathi, Sudharsan"
+}
