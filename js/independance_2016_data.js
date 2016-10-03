f4dad0d16ad72c25d40855f2e6bf3c9dc06024ea
Sudharsan Vaidhun <svqm5@mst.edu>
2016-10-03 14:53:30 -0500

    Resize Images Complete

--- js/independance_2016_data.js
+++ js/independance_2016_data.js
@@ -3,38 +3,47 @@ var independace_data = {
 	photo : [
 		{
 			image_src : "img/independance/image01.jpg",
+			image_small : "img/independance/small/image-small-01.jpg",
 			name : "..."
 		},
 		{
 			image_src : "img/independance/image02.jpg",
+			image_small : "img/independance/small/image-small-02.jpg",
 			name : "..."
 		},
 		{
 			image_src : "img/independance/image03.jpg",
+			image_small : "img/independance/small/image-small-03.jpg",
 			name : "..."
 		},
 		{
 			image_src : "img/independance/image04.jpg",
+			image_small : "img/independance/small/image-small-04.jpg",
 			name : "..."
 		},
 		{
 			image_src : "img/independance/image05.jpg",
+			image_small : "img/independance/small/image-small-05.jpg",
 			name : "..."
 		},
 		{
 			image_src : "img/independance/image06.jpg",
+			image_small : "img/independance/small/image-small-06.jpg",
 			name : "..."
 		},
 		{
 			image_src : "img/independance/image07.jpg",
+			image_small : "img/independance/small/image-small-07.jpg",
 			name : "..."
 		},
 		{
 			image_src : "img/independance/image08.jpg",
+			image_small : "img/independance/small/image-small-08.jpg",
 			name : "..."
 		},
 		{
 			image_src : "img/independance/image09.jpg",
+			image_small : "img/independance/small/image-small-09.jpg",
 			name : "..."
 		}
 	],
