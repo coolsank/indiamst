f2744d8c404cae0d860c410f4cae910d74f6a22b
prithvivihari11 <prithvivihari11@gmail.com>
2017-06-23 14:58:30 -0500

    Update Independence Day

--- js/independance_2016_data.js
+++ js/independance_2016_data.js
@@ -2,50 +2,30 @@ var independace_data = {
 	album_name : "Independence Day 2016",
 	photo : [
 		{
-			image_src : "img/independance/image01.jpg",
-			image_small : "img/independance/small/image-small-01.jpg",
+			image_src : "img/independance/ind_1.jpg",
+			image_small : "img/independance/small/ind_1.jpg",
 			name : "..."
 		},
 		{
-			image_src : "img/independance/image02.jpg",
-			image_small : "img/independance/small/image-small-02.jpg",
+			image_src : "img/independance/ind_2.jpg",
+			image_small : "img/independance/small/ind_2.jpg",
 			name : "..."
 		},
 		{
-			image_src : "img/independance/image03.jpg",
-			image_small : "img/independance/small/image-small-03.jpg",
+			image_src : "img/independance/ind_3.jpg",
+			image_small : "img/independance/small/ind_3.jpg",
 			name : "..."
 		},
 		{
-			image_src : "img/independance/image04.jpg",
-			image_small : "img/independance/small/image-small-04.jpg",
+			image_src : "img/independance/ind_4.jpg",
+			image_small : "img/independance/small/ind_4.jpg",
 			name : "..."
 		},
 		{
-			image_src : "img/independance/image05.jpg",
-			image_small : "img/independance/small/image-small-05.jpg",
-			name : "..."
-		},
-		{
-			image_src : "img/independance/image06.jpg",
-			image_small : "img/independance/small/image-small-06.jpg",
-			name : "..."
-		},
-		{
-			image_src : "img/independance/image07.jpg",
-			image_small : "img/independance/small/image-small-07.jpg",
-			name : "..."
-		},
-		{
-			image_src : "img/independance/image08.jpg",
-			image_small : "img/independance/small/image-small-08.jpg",
-			name : "..."
-		},
-		{
-			image_src : "img/independance/image09.jpg",
-			image_small : "img/independance/small/image-small-09.jpg",
+			image_src : "img/independance/ind_5.jpg",
+			image_small : "img/independance/small/ind_5.jpg",
 			name : "..."
 		}
 	],
-	image_credits : "Swathi, Sudharsan"
+	image_credits : "Prithvi Vihari"
 }
